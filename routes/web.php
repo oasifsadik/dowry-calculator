9e4c4eda39117fb9e1d9ce5af72803702d391b1b
oasifsadik <oasifsadik99@gmail.com>
2026-04-11 00:01:47 +0600

    add

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,11 @@ use App\Http\Controllers\JoutukController;
 Route::get('/', [JoutukController::class, 'index'])->name('joutuk.index');
 Route::post('/calculate', [JoutukController::class, 'calculate'])->name('joutuk.calculate');
 Route::get('/result',     [JoutukController::class, 'result'])->name('joutuk.result');
+Route::get('/clear-all', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('view:clear');
+    Artisan::call('config:cache');
+    Artisan::call('optimize:clear');
+    Artisan::call('storage:link');
+    dd('All Cleared...');
+});
